c6d9ee2c193b2f9ba5578000501c942ac94a9734
RobertoGG <rguerreroyu@gmail.com>
2025-12-15 13:44:55 -0800

    Connected to database(#1)

--- api/src/main/java/org/yearup/VinlyCountdownRecordsApplication.java
+++ api/src/main/java/org/yearup/VinlyCountdownRecordsApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EasyshopApplication
+public class VinlyCountdownRecordsApplication
 {
 
     public static void main(String[] args) {
-        SpringApplication.run(EasyshopApplication.class, args);
+        SpringApplication.run(VinlyCountdownRecordsApplication.class, args);
     }
 
 
